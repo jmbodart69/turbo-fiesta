67acd869363dc258f579a372fed51792aea71d5e
Jmbodart69 <jmbodart69@gmail.com>
2022-09-13 15:12:26 +0000

    initial git setup

---
+++ setup_git.R
@@ -0,0 +1,10 @@
+library(usethis)
+use_git_config(user.name = Sys.getenv("user_name"), 
+               user.email = Sys.getenv("user_email"))
+
+# 1. create the token
+usethis::create_github_token()
+
+# 2. Redirect to browser and create the token.
+# 3. Go back to R session and execute:
+credentials::set_github_pat(FALSE)
